3ee584edda0316be388257834a8e7f0be4ca2217
Emmanuel <ezeemmanuel2010@gmail.com>
2019-07-05 22:48:35 +0100

    feature(api): create user sign up api endpoint
    [Start #167113377]

---
+++ src/controllers/userController.js
@@ -0,0 +1,40 @@
+import { UserModel } from '../models';
+import { feedbackHandler } from '../Handlers';
+import jwtGenerator from '../middleware/jwtGenerator';
+
+class userController {
+  static async signUp(req, res, next) {
+    try {
+      const user = await UserModel.create(req.body);
+      // if (UserModel.emailExist(user.email)) {
+      //   return;
+      // }
+      const token = await jwtGenerator.generateToken({
+        id: user.id,
+        email: user.email,
+        isAdmin: user.is_admin,
+      });
+      const data = { token, ...user };
+      feedbackHandler.message(res, data, 201);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async signIn(req, res, next) {
+    try {
+      const user = await UserModel.getUserByEmail(req.body);
+      const token = await jwtGenerator.generateToken({
+        id: user.id,
+        email: user.email,
+        isAdmin: user.is_admin,
+      });
+      const data = { token, ...user };
+      feedbackHandler.message(res, data, 200);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+export default userController;
---
+++ src/models/userModel.js
@@ -0,0 +1,80 @@
+/* eslint-disable camelcase */
+import bcrypt from 'bcrypt';
+import DB from '../DB';
+import { ErrorHandler } from '../Handlers';
+
+class UserModel {
+  static async create({
+    email, first_name, last_name, password,
+  }) {
+    if (!(email && password)) {
+      throw new ErrorHandler('Enter email and password');
+    }
+    const is_admin = true;
+    const query = `
+    INSERT INTO users (
+      email,
+      first_name,
+      last_name,
+      password,
+      is_admin
+    )
+    VALUES($1,$2,$3,$4,$5)
+      RETURNING *
+    `;
+
+    const hashedPassword = await bcrypt.hash(password, 15);
+    const params = [email, first_name, last_name, hashedPassword, is_admin];
+    const user = await DB.query(query, params);
+    return UserModel.exclude(user);
+  }
+
+  static async getUserById(id) {
+    const query = `
+      SELECT * FROM users
+        WHERE 
+          id = $1
+    `;
+    const param = [id];
+    const user = await DB.query(query, param);
+    return UserModel.exclude(user);
+  }
+
+  static async getUserByEmail({ email, password }) {
+    const query = `
+    SELECT * FROM users
+      WHERE
+        email = $1`;
+
+    const param = [email];
+    const user = await DB.query(query, param);
+    if (!user) {
+      throw new ErrorHandler('incorrect username or password');
+    }
+    const isPassword = bcrypt.compare(password, user.password);
+    if (!isPassword) {
+      throw new ErrorHandler('incorrect username or password');
+    }
+    return UserModel.exclude(user);
+  }
+
+  static async emailExist(email) {
+    const query = `
+    SELECT * FROM users
+      WHERE 
+        email = $1
+    `;
+    const param = [email];
+    const user = await DB.query(query, param);
+    if (user) {
+      throw new ErrorHandler('Email already exists', 401);
+    }
+    return user;
+  }
+
+  static exclude({ password, ...otherColumns }) {
+    return otherColumns;
+  }
+}
+
+export default UserModel;
